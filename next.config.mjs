58fea677a0f170f660c45586baafa4e7e3f9bc2e
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2025-11-07 12:51:51 +0200

    fixed remote image loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [new URL('https://image.tmdb.org/t/p/w500/**')],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+    ],
   },
 };
 
